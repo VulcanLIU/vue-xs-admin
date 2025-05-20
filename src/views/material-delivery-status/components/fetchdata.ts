0276f048566eda3e458b24ab803055855d25eabf
VulcanLiu <VulcanLiu@outlook.com>
2025-05-20 19:26:43 +0800

    20250520-2

--- src/views/material-delivery-status/components/fetchdata.ts
+++ src/views/material-delivery-status/components/fetchdata.ts
@@ -6,59 +6,59 @@ import type { ProductItem, RawData } from "../types/material";
 
 //|-数据获取-API调用函数
 export const fetchData = async (args: string = "") => {
- // eslint-disable-next-line no-var
- var raw_data: RawData[] = [];
- const raw_nesting_tabList = ref<ProductItem[]>([]);
- const nesting_tabList = ref<ProductItem[]>([]);
- try {
-  //-|-使用API获取数据
-  const response = await axios.get<RawData[]>(
-   `http://localhost:5000/api/perchase_data${args}`,
-  );
+	// eslint-disable-next-line no-var
+	var raw_data: RawData[] = [];
+	const raw_nesting_tabList = ref<ProductItem[]>([]);
+	const nesting_tabList = ref<ProductItem[]>([]);
+	try {
+		//-|-使用API获取数据
+		const response = await axios.get<RawData[]>(
+			`http://localhost:5000/api/perchase_data${args}`,
+		);
 
-  //-|-如果API无传入数据则是在索取全部数据-保存在raw_data变量中
-  if (args === "") {
-   raw_data = response.data.data;
-   raw_nesting_tabList.value = rawData_2_ProductItemList(response.data.data);
-  }
-  nesting_tabList.value = rawData_2_ProductItemList(response.data.data);
- } catch (error) {
-  console.error("数据获取失败:", error);
- }
- return [raw_data, raw_nesting_tabList.value, nesting_tabList.value];
+		//-|-如果API无传入数据则是在索取全部数据-保存在raw_data变量中
+		if (args === "") {
+			raw_data = response.data.data;
+			raw_nesting_tabList.value = rawData_2_ProductItemList(response.data.data);
+		}
+		nesting_tabList.value = rawData_2_ProductItemList(response.data.data);
+	} catch (error) {
+		console.error("数据获取失败:", error);
+	}
+	return [raw_data, raw_nesting_tabList.value, nesting_tabList.value];
 };
 
 //
 export const upsertParts = async (part: Partial<RawData>) => {
- try {
-  const response = await axios.post(
-   `http://localhost:5000/api/upsert_part`,
-   part,
-  );
-  if (response.status === 200) {
-   console.log("数据更新成功:", response.data);
-  }
-  // 处理成功的响应
-  return true;
- } catch (error) {
-  console.error("数据更新失败:", error);
-  return false;
- }
+	try {
+		const response = await axios.post(
+			`http://localhost:5000/api/upsert_part`,
+			part,
+		);
+		if (response.status === 200) {
+			console.log("数据更新成功:", response.data);
+		}
+		// 处理成功的响应
+		return true;
+	} catch (error) {
+		console.error("数据更新失败:", error);
+		return false;
+	}
 };
 
 export const deleteParts = async (part: Partial<RawData>) => {
- try {
-  const response = await axios.post(
-   `http://localhost:5000/api/delete_part`,
-   part,
-  );
-  if (response.status === 200) {
-   console.log("数据更新成功:", response.data);
-  }
-  // 处理成功的响应
-  return true;
- } catch (error) {
-  console.error("数据更新失败:", error);
-  return false;
- }
+	try {
+		const response = await axios.post(
+			`http://localhost:5000/api/delete_part`,
+			part,
+		);
+		if (response.status === 200) {
+			console.log("数据更新成功:", response.data);
+		}
+		// 处理成功的响应
+		return true;
+	} catch (error) {
+		console.error("数据更新失败:", error);
+		return false;
+	}
 };
--- src/views/material-delivery-status/components/form/index.vue
+++ src/views/material-delivery-status/components/form/index.vue
@@ -1,106 +1,106 @@
 <script lang="ts" setup generic="T extends Object">
-import Form from "@/components/Form/index.vue";
 import { h, reactive, ref, shallowRef } from "vue";
+import Form from "@/components/Form/index.vue";
 import type { FormProps } from "@/components/Form/types/from";
 //componentA组件
 export interface FormDataType {
- product_name?: string;
- product_number?: string;
- dateTime?: string;
- cascader?: string[];
- select?: string;
- checkbox?: string[];
- radio?: string;
- slotInput?: string;
- renderInput?: string;
- admin?: number;
+	product_name?: string;
+	product_number?: string;
+	dateTime?: string;
+	cascader?: string[];
+	select?: string;
+	checkbox?: string[];
+	radio?: string;
+	slotInput?: string;
+	renderInput?: string;
+	admin?: number;
 }
 const emit = defineEmits<{
- (e: "submitForm", form_1: FormDataType): boolean;
+	(e: "submitForm", form_1: FormDataType): boolean;
 }>();
 
 const form = ref<FormDataType>({ product_name: "" });
 
 const formOption = reactive<FormProps<FormDataType>>({
- labelPosition: "right",
- formItem: [
-  {
-   gutter: 30,
-   itemList: [
-    {
-     component: "ElInput",
-     label: "产品名称",
-     prop: "product_name",
-     rules: [{ required: false, type: "string" }],
-     props: {
-      onChange: (e: string) => {
-       console.log(e);
-      },
-     },
-    },
-    {
-     component: "ElInput",
-     label: "产品图号",
-     prop: "product_number",
-     rules: [{ required: false, type: "string" }],
-     props: {
-      onChange: (e: string) => {
-       console.log(e);
-      },
-     },
-    },
-   ],
-  },
- ],
+	labelPosition: "right",
+	formItem: [
+		{
+			gutter: 30,
+			itemList: [
+				{
+					component: "ElInput",
+					label: "产品名称",
+					prop: "product_name",
+					rules: [{ required: false, type: "string" }],
+					props: {
+						onChange: (e: string) => {
+							console.log(e);
+						},
+					},
+				},
+				{
+					component: "ElInput",
+					label: "产品图号",
+					prop: "product_number",
+					rules: [{ required: false, type: "string" }],
+					props: {
+						onChange: (e: string) => {
+							console.log(e);
+						},
+					},
+				},
+			],
+		},
+	],
 });
 
 const handlerForm = async (val: string) => {
- if (val === "vertical") {
-  formOption.formItem.map((res) => {
-   res.md = 24;
-   res.lg = 24;
-   res.xl = 24;
-   return res;
-  });
- } else if (val === "horizontal") {
-  formOption.formItem.map((res) => {
-   delete res.md;
-   delete res.lg;
-   delete res.xl;
-   return res;
-  });
- }
+	if (val === "vertical") {
+		formOption.formItem.map((res) => {
+			res.md = 24;
+			res.lg = 24;
+			res.xl = 24;
+			return res;
+		});
+	} else if (val === "horizontal") {
+		formOption.formItem.map((res) => {
+			delete res.md;
+			delete res.lg;
+			delete res.xl;
+			return res;
+		});
+	}
 };
 handlerForm("horizontal");
 
 //此处是检索，再通过emit发送给父组件
 const submitForm = (value: FormDataType) => {
- emit("submitForm", shallowRef(value).value);
+	emit("submitForm", shallowRef(value).value);
 };
 
 const resetForm = (value: FormDataType) => {
- for (const item in form.value) {
-  form.value[item] = "";
-  //console.log(item);
- }
- emit("submitForm", shallowRef(value).value);
+	for (const item in form.value) {
+		form.value[item] = "";
+		//console.log(item);
+	}
+	emit("submitForm", shallowRef(value).value);
 };
 </script>
 
 <template>
- <div class="page-container">
-  <Form
-   ref="formRef"
-   :form-data="form"
-   :form-option="formOption"
-   @submit-form="submitForm"
-   @reset-form="resetForm"
-  />
- </div>
+	<div class="page-container">
+		<Form
+			ref="formRef"
+			:form-data="form"
+			:form-option="formOption"
+			@submit-form="submitForm"
+			@reset-form="resetForm"
+		/>
+	</div>
 </template>
 
 <style lang="scss" scoped>
 .config {
- margin-bottom: 20px;
+	margin-bottom: 20px;
 }
 </style>
